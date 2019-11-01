b657dd634616224093a22292169953b1eed6c0e5
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2019-11-01 21:12:58 +1100

    Initial set-up folders/files

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
