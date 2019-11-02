5fe3a977c037077d177185ed963804832402bd55
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2019-11-02 19:54:45 +1100

    Update controller

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -14,22 +14,28 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/", function (req, res) {
-    burger.insertOne(req.body.burger_name, function () {
-        res.redirect("/");
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name,
+        req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId });
     });
 });
 
-router.put("/:id", function (req, res) {
-    var id = req.params.id;
-
-    console.log("id", id);
-
-    burger.updateOne(id, function () {
-        res.redirect("/");
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
 });
-
-
 
 module.exports = router;
