9214c6163c4476c3d78d70b5be726101e775f52e
Elisabeth Loeser <19419418+ElliLoes@users.noreply.github.com>
2019-11-02 23:29:59 +1100

    Add JawsDB MySQL

--- server.js
+++ server.js
@@ -24,4 +24,24 @@ var routes = require("./controllers/burger_controller.js");
 
 app.use("/", routes);
 
+var mysql = require('mysql');
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: 'clubben.1',
+        database: 'burgers_db'
+    })
+};
+
+connection.connect();
+module.exports = connection;
+
 app.listen(port);
